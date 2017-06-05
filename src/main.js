dbe7d34fb4970112ca2ddfeb6dcb115f1767c721
hufenghuai <13521858457@163.com>
2017-06-05 10:39:09 +0800

    vue-music

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import 'babel-polyfill';
+import fastclick from 'fastclick';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+fastclick.attach(document.body);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App },
+});
